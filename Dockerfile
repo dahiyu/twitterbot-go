169b929628d1d031c853cde9a7f8e9b62f033c24
Daichi Yamashita <dahiyu07@gmail.com>
2019-11-10 16:11:27 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Build image
+FROM golang:latest AS builder
+
+# Install dependencies
+WORKDIR /go/src/twitter-bot
+RUN go get github.com/ChimeraCoder/anaconda && \
+    go get github.com/joho/godotenv && \
+    go get github.com/mmcdole/gofeed && \
+    go get github.com/jasonlvhit/gocron
+
+# Build modules
+COPY main.go .
+COPY .env .
+RUN GOOS=linux CGO_ENABLED=0 go build main.go
+
+#--
+
+# Production image
+FROM busybox
+WORKDIR /opt/twitter-bot/bin
+
+# Deploy modules
+COPY --from=builder /go/src/twitter-bot .
+ENV TZ=Asia/Tokyo
+COPY crontab /var/spool/cron/crontabs/root
+# ENTRYPOINT ["/opt/twitter-bot/bin/main"]
+# CMD ["crond" "-f", "-d", "8"]
+CMD crond -f -d 8
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/joho/godotenv"
+	"github.com/mmcdole/gofeed"
+)
+
+func main() {
+	// loadEnv()
+
+	// scheduler := gocron.NewScheduler()
+	// scheduler.Every(2).Minutes().Do(task)
+
+	api := getTwitterApi()
+	fp := gofeed.NewParser()
+
+	feed, _ := fp.ParseURL("http://b.hatena.ne.jp/hotentry/it.rss")
+
+	for _, item := range feed.Items {
+		if !tweeted(item.Title, api) {
+			// tweet, _ := api.PostTweet(item.Title+"\n"+item.Link, nil)
+			// fmt.Println(tweet)
+			fmt.Println(item.Title)
+			return
+		}
+	}
+}
+
+func getTwitterApi() *anaconda.TwitterApi {
+	anaconda.SetConsumerKey(os.Getenv("CONSUMER_KEY"))
+	anaconda.SetConsumerSecret(os.Getenv("CONSUMER_SECRET"))
+	return anaconda.NewTwitterApi(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+// 取得したタイトルをすでにツイートしているか判定
+func tweeted(title string, api *anaconda.TwitterApi) bool {
+	v := url.Values{}
+	v.Set("user_id", "1093431019930247168")
+	v.Set("count", "100")
+	tweets, _ := api.GetUserTimeline(v)
+	for _, tweet := range tweets {
+		if strings.Contains(tweet.FullText, title) {
+			return true
+		}
+	}
+	return false
+}
